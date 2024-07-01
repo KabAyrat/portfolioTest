72cfcee39ffd3c60088a0e0727e81bf9ac4521ad
KabAyrat <kabirov.ayrat2004@gmail.com>
2024-07-01 14:13:33 +0300

    небольшие корректировки

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
       document.querySelector(".loader").style.display = "none";
       document.querySelector(".content").style.display = "block";
-    }, 700);
+    }, 700);  // изменять время подгузки здесь!
   });
